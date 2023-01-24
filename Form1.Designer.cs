0d2dc0be6b0d2ca97d7b3f36c01cccf068ca2b2f
Alphonse <alphonseja@gmail.com>
2023-01-24 17:24:51 +0530

    design change

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,9 +44,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(363, 123);
+            this.textBox1.Location = new System.Drawing.Point(181, 123);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.Size = new System.Drawing.Size(429, 23);
             this.textBox1.TabIndex = 1;
             // 
             // Form1
